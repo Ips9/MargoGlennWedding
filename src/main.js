3fdf0e2f12060ad0e8e582aabd35a4c864b95a48
Ips9 <85155418+Ips9@users.noreply.github.com>
2026-09-02 23:00:57 +0200

    Update main.js
    New intro

--- src/main.js
+++ src/main.js
@@ -245,8 +245,8 @@ document.querySelector('#app').innerHTML = `
         </h1>
 
         <p class="intro">
-          Op 2 oktober 2027 vieren we samen met jullie
-          de liefde, het leven en alles wat nog komt.
+          Op 2 oktober 2027 vieren we graag samen met jullie
+          onze liefde en de start van onze toekomst als man en vrouw.
         </p>
 
         <div class="hero-actions">
